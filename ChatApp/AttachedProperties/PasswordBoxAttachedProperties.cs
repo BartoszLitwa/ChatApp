29e4f7000538bb823295f9c73fb7f98d3a841d6c
Bartosz Litwa <mc.vargtimmen@gmail.com>
2019-12-18 22:15:39 +0100

    18.12.2019(7 poczatek)

---
+++ ChatApp/AttachedProperties/PasswordBoxAttachedProperties.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ChatApp
+{
+    /// <summary>
+    /// The MonitorPassword attached property for a <see cref="PasswordBox"/> 
+    /// </summary>
+    public class MonitorPasswordProperty : BaseAttachedProperty<MonitorPasswordProperty, bool> 
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Get the caller
+            var passwordBox = sender as PasswordBox;
+
+            //Make sure its password box
+            if (passwordBox == null)
+                return;
+
+            // Remove any previous events
+            passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+
+            // If the caller set MonitorPassword to true
+            if ((bool)e.NewValue)
+            {
+                HasTextProperty.SetValue(passwordBox);
+                // Start listening out
+                passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+            }
+        }
+
+        /// <summary>
+        /// Fired when the passwordBox password value changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            HasTextProperty.SetValue((PasswordBox)sender);
+        }
+    }
+
+    /// <summary>
+    /// Has text attached property for a <see cref="PasswordBox"/>
+    /// </summary>
+    public class HasTextProperty : BaseAttachedProperty<HasTextProperty, bool> 
+    {
+        /// <summary>
+        /// Sets the HasText based on if the caller <see cref="PasswordBox"/> has any text
+        /// </summary>
+        /// <param name="sender"></param>
+        public static void SetValue(DependencyObject sender)
+        {
+            SetValue(sender, ((PasswordBox)sender).SecurePassword.Length > 0);
+        }
+    }
+}
---
+++ ChatApp/ValueConverters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace ChatApp
+{
+    /// <summary>
+    /// A converter that takes in a bollean and returns a <see cref="Visibility"/>
+    /// </summary>
+    public class BooleanToVisibilityConverter : BaseValueConverter<BooleanToVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
